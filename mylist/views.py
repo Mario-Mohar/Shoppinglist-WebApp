5d1a3701ada7219fc0a29f6984efad797e7f1d5f
TheMo-at <iam@themo.at>
2023-01-23 21:34:35 +0100

    kleine änderung

--- mylist/views.py
+++ mylist/views.py
@@ -7,3 +7,11 @@ def mylist(request):
         shoppingItem.objects.create(name = request.POST['name'])
     all_items = shoppingItem.objects.all()
     return render(request, "shoppinglist.html", {"all_items": all_items}) 
+
+def delete_item(request, item_name):
+    try:
+        item = shoppingItem.objects.get(name=item_name)
+        item.delete()
+    except shoppingItem.DoesNotExist:
+        return HttpResponse(f'Item with name {item_name} not found', status=404)
+    return HttpResponse(f'Item with name {item_name} deleted successfully')
